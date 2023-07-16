0181b58626c6d610e793683e89885a65edc6683e
nguyen thanh luu <nguyenthanhluu@gmail.com>
2023-07-16 15:30:51 +0700

    fix: change a tag link on UI

--- src/index.ts
+++ src/index.ts
@@ -8,12 +8,9 @@ app.use("/api/images", ImageRouter);
 
 app.use("/", (req, res): void => {
   res.send(
-    `You can access link below to get image with width and height:
-      <ul>
-        <li>
-          <a href="/api/images?fileName=tayson&width=400&height=200">/api/images?filename=tayson&width=400&height=200</a>
-        </li>
-      </ul>`,
+    `You can access link to get image with width and height:
+      <a href="/api/images?fileName=tayson&width=400&height=200">/api/images?filename=tayson&width=400&height=200</a>
+    `,
   );
 });
 
